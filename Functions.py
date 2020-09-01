d37f64b92dd8863032dcf7c91ce28a871f09e22b
Austin <aj_viens_deruisseau@yahoo.com>
2020-09-01 15:01:42 -0400

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -40,11 +40,42 @@ def K2F(a):
 def SqK2SqM(a):
     return round(a/2.59,4)
 def Acre2SqM(a):
-    return (round(a/640,4)
-            
+    return (round(a/640),4)
 
 
+def ARectangle(a,b):
+    return (a*b)
+def ASquare(a):
+    return (a**2)
+def ACircle(a):
+    return round(3.145192*(a**2),4)
+def AEllipse(a,b):
+    return round(3.141592*a*b,4)
+def ATriangle(a,b):
+    return round(.5*(a*b),3)
 
+def Square4Root(n, i, j): 
+    mid = (i + j) / 2; 
+    mul = mid * mid; 
+    if ((mul == n) or (abs(mul - n) < 0.00001)): 
+        return mid; 
+    elif (mul < n): 
+        return Square4Root(n, mid, j); 
+    else: 
+        return Square4Root(n, i, mid); 
+def SquareRoot(a):
+    i = 1; 
+    found = False; 
+    while(found == False): 
+         if (i * i == a): 
+            return i
+            found = True;       
+         elif(i * i > a): 
+            res = Square4Root(a, i - 1, i); 
+            return round(res,4)
+            found = True;
+         i += 1;
+    
 
 
 
